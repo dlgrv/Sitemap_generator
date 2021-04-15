af7a78d9941fe3934ba4839dc851919b987ef87d
dlgrv <68144056+dlgrv@users.noreply.github.com>
2021-04-15 22:48:26 +0300

    V1.0

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import logging
 import time
 
 links = {
-    'craw': {'link': 'http://crawler-test.com/', 'time': 0, 'count_links': 0},
-    'goog': {'link': 'http://google.com/', 'time': 0, 'count_links': 0},
-    'vk': {'link': 'https://vk.com/', 'time': 0, 'count_links': 0},
-    #'yndx': {'link': 'https://yandex.ru/', 'time': 0, 'count_links': 0},
-    'stckovrfl': {'link': 'https://stackoverflow.com/', 'time': 0, 'count_links': 0}
+    'craw': {'link': 'http://crawler-test.com/', 'count_links': 0},
+    'goog': {'link': 'http://google.com/', 'count_links': 0},
+    'vk': {'link': 'https://vk.com/', 'count_links': 0},
+    #'yndx': {'link': 'https://yandex.ru/', 'count_links': 0},
+    'stckovrfl': {'link': 'https://stackoverflow.com/', 'count_links': 0}
 }
 
 
@@ -34,17 +34,6 @@ def count_lines(filename, chunk_size=1<<13):
 if __name__ == '__main__':
     procs = []
     list_link_keys = links.keys()
-
-    for link_key in list_link_keys:
-        proc = Process(target=sitemap_gen, args=(links[link_key]['link'], link_key))
-        start_time = time.time()
-        proc.start()
-        proc.join()
-        stop_time = time.time()
-        links[link_key]['time'] = format(stop_time - start_time, '.2f')
-
-    '''
-    # Данный способ гораздо эффективнее, но при нем я не понимаю, какой использовать метод для подсчета времени -_(^^)_-
     for link_key in links:
         proc = Process(target=sitemap_gen, args=(links[link_key]['link'], link_key))
         procs.append(proc)
@@ -52,9 +41,8 @@ if __name__ == '__main__':
 
     for proc in procs:
         proc.join()
-    '''
 
-    col_labels = ['URL сайта', 'Время обработки (сек)', 'Кол-во найденных ссылок', 'Имя файла с результатом']
+    col_labels = ['URL сайта', 'Кол-во найденных ссылок', 'Имя файла с результатом']
     data = []
 
     fig, ax = plt.subplots()
@@ -62,7 +50,6 @@ if __name__ == '__main__':
     for link_key in list_link_keys:
         links[link_key]['count_links'] = count_lines(f'sitemap_{link_key}.xml') - 2
         data.append([links[link_key]['link'],
-                     f"{links[link_key]['time']}",
                      links[link_key]['count_links'],
                      f'sitemap_{link_key}.xml'])
 
@@ -73,5 +60,3 @@ if __name__ == '__main__':
 
     plt.savefig('result.png',
                 dpi=250)
-
-    plt.show()
